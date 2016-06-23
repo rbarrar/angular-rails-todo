15128398aa5e7ac2ac3cca1390019d514b9e7e41
Ryan Barrar <Rbarrar17@gmail.com>
2016-06-23 18:00:17 -0400

    got the html for the todo component and todocomponent.ts to work so that I can now update my todo item

--- frontend/dist/app/todo-item/todo-item.component.js
+++ frontend/dist/app/todo-item/todo-item.component.js
@@ -9,10 +9,31 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var todo_item_service_1 = require('../todo-item.service');
+var router_deprecated_1 = require('@angular/router-deprecated');
+var router_deprecated_2 = require('@angular/router-deprecated');
 var TodoItemComponent = (function () {
-    function TodoItemComponent() {
+    function TodoItemComponent(todo_itemService, routeParams, router) {
+        this.todo_itemService = todo_itemService;
+        this.routeParams = routeParams;
+        this.router = router;
     }
     TodoItemComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        if (this.routeParams.get('id') !== null) {
+            var id = +this.routeParams.get('id');
+            this.todo_itemService.getTodoItem(id)
+                .then(function (todo_item) { return _this.todo_item = todo_item; });
+        }
+    };
+    TodoItemComponent.prototype.save = function () {
+        var _this = this;
+        this.todo_itemService
+            .save(this.todo_item)
+            .then(function (todo_item) {
+            _this.router.navigate(['TodoItems']);
+        })
+            .catch(function (error) { return _this.error = error; }); // TODO: Display error message
     };
     TodoItemComponent = __decorate([
         core_1.Component({
@@ -21,7 +42,7 @@ var TodoItemComponent = (function () {
             templateUrl: 'todo-item.component.html',
             styleUrls: ['todo-item.component.css']
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [todo_item_service_1.TodoItemsService, router_deprecated_1.RouteParams, router_deprecated_2.Router])
     ], TodoItemComponent);
     return TodoItemComponent;
 }());
--- frontend/src/app/todo-item/todo-item.component.ts
+++ frontend/src/app/todo-item/todo-item.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoItemsService } from '../todo-item.service';
+import { RouteParams } from '@angular/router-deprecated';
+import { TodoItem } from '../models/todo_item';
+import { Router } from '@angular/router-deprecated';
 
 @Component({
   moduleId: module.id,
@@ -7,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
+  todo_item: TodoItem;
+  error: any;
 
-  constructor() {}
+  constructor (
+    private todo_itemService: TodoItemsService,
+    private routeParams: RouteParams,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    if (this.routeParams.get('id') !== null) {
+      let id = +this.routeParams.get('id');
+      this.todo_itemService.getTodoItem(id)
+      .then(todo_item => this.todo_item = todo_item);
+    }
   }
-
+  save() {
+    this.todo_itemService
+      .save(this.todo_item)
+      .then(todo_item => {
+        this.router.navigate(['TodoItems']);
+      })
+      .catch(error => this.error = error); // TODO: Display error message
+  }
 }
